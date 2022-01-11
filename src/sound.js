ead0111ce11e126b3a392178f8f81976b99fc160
PinkiePieIsTheBestPony <outbackgaminghd@hotmail.com>
2022-01-12 01:50:52 +1100

    Add new youtube playlist functionality, along with allowing extra args to impact sound commands

--- src/sound.js
+++ src/sound.js
@@ -1,12 +1,14 @@
 const dbQuery = require('./db/dbQuery');
 const { joinVoiceChannel, getVoiceConnection, VoiceConnectionStatus, entersState, createAudioPlayer, createAudioResource, AudioPlayerStatus, StreamType } = require('@discordjs/voice');
 const ytdl = require("ytdl-core");
+const ytpl = require('ytpl');
 const discord = require('./external-libs/discord');
 
 let allAudioPlayers = '';
 let allPauseStates = '';
 let allPrevSongs = '';
 let allStates = '';
+let allOptsGbl = '';
 
 function accessQueue(serverID) {
     return dbQuery.selectAllStatementDB("queue_data", "p_queue", ["server_id"], "=", [serverID]);
@@ -40,7 +42,7 @@ function prevSong(queue, serverID, msg) {
         updateQueue(serverID, JSON.stringify(queue));
         playQueue(serverID, msg);
     } else {
-        msg.type.reply("No previous songs!");
+        (allOptsGbl.notif) ? msg.type.reply("No previous songs!") : ((!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND")) ? msg.type.reply({ephemeral: true, content: "No previous songs!"}) : "";
     }
 }
 
@@ -64,7 +66,7 @@ async function playQueue(serverID, msg) {
         }));
 
         audioPlayer.on(AudioPlayerStatus.Playing, () => {
-            msg.channel.send(`Now playing: ${queue[0].title}`);
+            if (allOptsGbl.notif) msg.channel.reply(`Now playing: ${queue[0].title}`)
         });
 
         audioPlayer.on(AudioPlayerStatus.Idle, () => {
@@ -85,9 +87,11 @@ async function playQueue(serverID, msg) {
         audioPlayer.on('error', error => {
             console.error(`Error: ${error}.`);
             if (error.message === "Status code: 403") {
+                let queueNew = accessQueue(serverID);
+                queueNew = JSON.parse(queueNew);
                 nextSong(queueNew, serverID, msg);
             } else {
-                msg.channel.send("Error encountered...going to disconnect and leave.");
+                (allOptsGbl.notif) ? msg.type.reply("Error encountered...going to disconnect and leave.") : ((!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND")) ? msg.type.reply({ephemeral: true, content: "Error encountered...going to disconnect and leave."}) : "";
                 safelyRemove(msg);
             }
         });
@@ -132,7 +136,8 @@ function voiceStatus(serverID, msg) {
     })
 }
 
-const join = (msg) => {
+const join = (msg, optArgs) => {
+    allOptsGbl = optArgs;
     let connection = getVoiceConnection(msg.guild.id);
     if (typeof connection == 'undefined') {
         if (msg.member.voice.channel) {
@@ -141,21 +146,21 @@ const join = (msg) => {
                 let serverID = msg.guild.id;
                 let data = accessQueue(serverID);
                 if (data === "noDataFoundInQueue") {
-                    msg.type.reply("You need to add a song into this queue...")
+                    (allOptsGbl.notif) ? msg.type.reply("You need to add a song into this queue...") : ((!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND")) ? msg.type.reply({ephemeral: true, content: "You need to add a song into this queue..."}) : "";
                 } else {
                     data = JSON.parse(data);
                     joinVoiceChannel({channelId: msg.member.voice.channel.id, guildId: serverID, adapterCreator: msg.guild.voiceAdapterCreator});
-                    msg.type.reply("Joining voice channel!");
+                    (allOptsGbl.notif) ? msg.type.reply("Joining voice channel!") : ((!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND")) ? msg.type.reply({ephemeral: true, content: "Joining voice channel!"}) : "";
                     voiceStatus(serverID, msg);
                 }
             } else {
-                msg.type.reply("Please give me the appropriate permissions to play sounds!");
+                (allOptsGbl.notif) ? msg.type.reply("Please give me the appropriate permissions to play sounds!") : ((!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND")) ? msg.type.reply({ephemeral: true, content: "Please give me the appropriate permissions to play sounds!"}) : "";
             }
         } else {
-            msg.type.reply("Must be in voice.");
+            (allOptsGbl.notif) ? msg.type.reply("Must be in voice.") : ((!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND")) ? msg.type.reply({ephemeral: true, content: "Must be in voice."}) : "";
         }
     } else {
-        msg.type.reply("I'm already in a server!")
+        (allOptsGbl.notif) ? msg.type.reply("I'm already in a server!") : ((!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND")) ? msg.type.reply({ephemeral: true, content: "I'm already in a server!"}) : "";
     }
 }
 
@@ -167,12 +172,14 @@ function safelyRemove(msg) {
     allStates.delete(msg.guild.id);
 }
 
-const leave = (msg) => {
-    msg.type.reply("Leaving voice channel!");
+const leave = (msg, optArgs) => {
+    allOptsGbl = optArgs;
+    (allOptsGbl.notif) ? msg.type.reply("Leaving voice channel!") : ((!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND")) ? msg.type.reply({ephemeral: true, content: "Leaving voice channel!"}) : "";
     safelyRemove(msg);
 }
 
-async function addToQueue(msg) {
+async function addToQueue(msg, optArgs) {
+    allOptsGbl = optArgs;
     let queue = accessQueue(msg.guild.id);
     if (queue === "noDataFoundInQueue") {
         queue = [];
@@ -185,18 +192,41 @@ async function addToQueue(msg) {
         let sound = {title: soundDetails.videoDetails.title, url: soundDetails.videoDetails.video_url};
         queue.push(sound);
         updateQueue(msg.guild.id, JSON.stringify(queue));
-        msg.type.reply("Added to the queue!");
+        (allOptsGbl.notif) ? msg.type.reply(`Added to the queue!`) : (!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND") ? msg.type.reply({ephemeral: true, content: `Added to the queue!`}) : "";
     } else {
-        msg.type.reply("Invalid URL. Please remember to only enter URLs from YT, and enter one at a time.")
+        (allOptsGbl.notif) ? msg.type.reply("Invalid URL. Please remember to only enter URLs from YT, and enter one at a time.") : ((!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND")) ? msg.type.reply({ephemeral: true, content: "Invalid URL. Please remember to only enter URLs from YT, and enter one at a time."}) : "";
     }
 }
 
-const removeFromQueue = (msg) => {
+async function addPlaylistToQueue(msg, optArgs) {
+    allOptsGbl = optArgs;
+    let queue = accessQueue(msg.guild.id);
+    if (queue === "noDataFoundInQueue") {
+        queue = [];
+    } else {
+        queue = JSON.parse(queue);
+    }
+    let isValidPlaylist = ytpl.validateID(msg.content);
+    if (isValidPlaylist) {
+        let playlist = await ytpl(msg.content);
+        for (let i = 0; i < playlist.items.length; i++) {
+            let sound = {title: playlist.items[i].title, url: playlist.items[i].shortUrl};
+            queue.push(sound);
+            updateQueue(msg.guild.id, JSON.stringify(queue));
+        }
+        (allOptsGbl.notif) ? msg.type.reply(`Added ${playlist.items.length} songs to the queue!`) : ((!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND")) ? msg.type.reply({ephemeral: true, content: `Added ${playlist.items.length} songs to the queue!`}) : "";
+    } else {
+        (allOptsGbl.notif) ? msg.type.reply("Invalid URL. Please remember to only enter playlist IDs from YT, and enter one at a time.") : ((!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND")) ? msg.type.reply({ephemeral: true, content: "Invalid URL. Please remember to only enter playlist IDs from YT, and enter one at a time."}) : "";
+    }
+}
+
+const removeFromQueue = (msg, optArgs) => {
+    allOptsGbl = optArgs;
     let queue = accessQueue(msg.guild.id);
     if (queue !== "noDataFoundInQueue") {
         queue = JSON.parse(queue);
         if (msg.content > queue.length) {
-            msg.type.reply("Invalid index for queue.")
+            (allOptsGbl.notif) ? msg.type.reply("Invalid index for queue.") : ((!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND")) ? msg.type.reply({ephemeral: true, content: "Invalid index for queue."}) : "";
         } else {
             queue.splice(msg.content, 1);
             if (queue.length > 0) {
@@ -204,19 +234,21 @@ const removeFromQueue = (msg) => {
             } else {
                 updateQueue(msg.guild.id, "noDataFoundInQueue");
             }
-            msg.type.reply("Queue updated!")
+            (allOptsGbl.notif) ? msg.type.reply("Queue updated!") : ((!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND")) ? msg.type.reply({ephemeral: true, content: "Queue updated!"}) : "";
         }
     } else {
-        msg.type.reply("Empty queue.")
+        (allOptsGbl.notif) ? msg.type.reply("Empty queue.") : ((!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND")) ? msg.type.reply({ephemeral: true, content: "Empty queue."}) : "";
     }
 }
 
-const clearQueue = (msg) => {
+const clearQueue = (msg, optArgs) => {
+    allOptsGbl = optArgs;
     updateQueue(msg.guild.id, "noDataFoundInQueue");
-    msg.type.reply("Queue cleared!");
+    (allOptsGbl.notif) ? msg.type.reply("Queue cleared!") : ((!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND")) ? msg.type.reply({ephemeral: true, content: "Queue cleared!"}) : "";
 }
 
-const next = (msg) => {
+const next = (msg, optArgs) => {
+    allOptsGbl = optArgs;
     if (allAudioPlayers !== '') {
         let queue = accessQueue(msg.guild.id);
         let audioPlayer = allAudioPlayers.get(msg.guild.id);
@@ -224,17 +256,18 @@ const next = (msg) => {
             allStates.set(msg.guild.id, 'N');           
             audioPlayer.stop();
             queue = JSON.parse(queue);
-            msg.type.reply("Playing next song in queue!");
+            (allOptsGbl.notif) ? msg.type.reply("Playing next song in queue!") : ((!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND")) ? msg.type.reply({ephemeral: true, content: "Playing next song in queue!"}) : "";
             nextSong(queue, msg.guild.id, msg);
         } else {
-            msg.type.reply("Need to have started playing music.")
+            (allOptsGbl.notif) ? msg.type.reply("Need to have started playing music.") : ((!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND")) ? msg.type.reply({ephemeral: true, content: "Need to have started playing music."}) : "";
         }
     } else {
-        msg.type.reply("Need to have started playing music.")
+        (allOptsGbl.notif) ? msg.type.reply("Need to have started playing music.") : ((!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND")) ? msg.type.reply({ephemeral: true, content: "Need to have started playing music."}) : "";
     }
 }
 
-const prev = (msg) => {
+const prev = (msg, optArgs) => {
+    allOptsGbl = optArgs;
     if (allAudioPlayers !== '') {
         let queue = accessQueue(msg.guild.id);
         let audioPlayer = allAudioPlayers.get(msg.guild.id);
@@ -242,17 +275,18 @@ const prev = (msg) => {
             allStates.set(msg.guild.id, 'P');
             audioPlayer.stop();
             queue = JSON.parse(queue);
-            msg.type.reply("Playing previous song in queue!");
+            (allOptsGbl.notif) ? msg.type.reply("Playing previous song in queue!") : ((!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND")) ? msg.type.reply({ephemeral: true, content: "Playing previous song in queue!"}) : "";
             prevSong(queue, msg.guild.id, msg);
         } else {
-            msg.type.reply("Need to have started playing music.");
+            (allOptsGbl.notif) ? msg.type.reply("Need to have started playing music.") : ((!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND")) ? msg.type.reply({ephemeral: true, content: "Need to have started playing music."}) : "";
         }
     } else {
-        msg.type.reply("Need to have started playing music.");
+        (allOptsGbl.notif) ? msg.type.reply("Need to have started playing music.") : ((!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND")) ? msg.type.reply({ephemeral: true, content: "Need to have started playing music."}) : "";
     }
 }
 
-const pause = (msg) => {
+const pause = (msg, optArgs) => {
+    allOptsGbl = optArgs;
     if (allAudioPlayers !== '') {
         let audio = allAudioPlayers.get(msg.guild.id)
         if (typeof audio !== 'undefined') {
@@ -266,25 +300,26 @@ const pause = (msg) => {
             }
             if (allPauseStates.get(msg.guild.id) === true) {
                 audio.unpause();
-                msg.type.reply("Unpausing the music!")
+                (allOptsGbl.notif) ? msg.type.reply("Unpausing the music!") : ((!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND")) ? msg.type.reply({ephemeral: true, content: "Unpausing the music!"}) : "";
                 allPauseStates.set(msg.guild.id, false);
             } else {
                 audio.pause();
-                msg.type.reply("Pausing the music!")
+                (allOptsGbl.notif) ? msg.type.reply("Pausing the music!") : ((!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND")) ? msg.type.reply({ephemeral: true, content: "Pausing the music!"}) : "";
                 allPauseStates.set(msg.guild.id, true);
             }
         } else {
-            msg.type.reply("Need to have started playing music.")
+            (allOptsGbl.notif) ? msg.type.reply("Need to have started playing music.") : ((!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND")) ? msg.type.reply({ephemeral: true, content: "Need to have started playing music."}) : "";
         }
     } else {
-        msg.type.reply("Need to have started playing music.")
+        (allOptsGbl.notif) ? msg.type.reply("Need to have started playing music.") : ((!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND")) ? msg.type.reply({ephemeral: true, content: "Need to have started playing music."}) : "";
     }
 }
 
-const showList = (msg) => {
+const showList = (msg, optArgs) => {
+    allOptsGbl = optArgs;
     let queue = accessQueue(msg.guild.id);
     if (queue === "noDataFoundInQueue") {
-        msg.type.reply("Nothing queued...")
+        (!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND") ? msg.type.reply({ephemeral: true, content: "Nothing queued..."}) : msg.type.reply("Nothing queued...")
     } else {
         if (allPrevSongs !== '') {
             let nextMsgQueue = JSON.parse(queue);
@@ -319,9 +354,9 @@ const showList = (msg) => {
                 }
             }
             
-            msg.type.reply({embeds: [discord.createQueueList(prevMsgQueue, nextMsgQueue, prevDetails, nextDetails, currentDetail)]});
+            (!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND") ? msg.type.reply({ephemeral: true, embeds: [discord.createQueueList(prevMsgQueue, nextMsgQueue, prevDetails, nextDetails, currentDetail)]}) : msg.type.reply({embeds: [discord.createQueueList(prevMsgQueue, nextMsgQueue, prevDetails, nextDetails, currentDetail)]});
         } else {
-            msg.type.reply("Needed to have started music...");
+            (!allOptsGbl.notif && msg.type.type === "APPLICATION_COMMAND") ? msg.type.reply({ephemeral: true, content: "Needed to have started music..."}) : msg.type.reply("Needed to have started music...")
         }
     }
 }
@@ -329,6 +364,7 @@ const showList = (msg) => {
 exports.join = join;
 exports.leave = leave;
 exports.addToQueue = addToQueue;
+exports.addPlaylistToQueue = addPlaylistToQueue;
 exports.removeFromQueue = removeFromQueue;
 exports.clearQueue = clearQueue;
 exports.showList = showList;
