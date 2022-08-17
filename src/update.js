c0a73eb589f03bc021441caf9a27239ba5c97b2e
PinkiePieIsTheBestPony <outbackgaminghd@hotmail.com>
2022-08-17 11:55:03 +1000

    Bug fixes and new column addition

--- src/update.js
+++ src/update.js
@@ -3,4 +3,5 @@ import {dbConnect} from './db/db.js';
 console.log("----START OF EXECUTION OF update.js----");
 let dbCon = dbConnect();
 dbCon.querySync("UPDATE p_broadcasts SET broadcast_valid = '1';");
+dbCon.querySync("ALTER TABLE p_fetcher ADD channel_name TEXT NOT NULL;");
 console.log("----END OF EXECUTION OF update.js----")
