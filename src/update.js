a8e45370efca290f1ae3e2ecdb5e8fb6ad6cadc3
PinkiePieIsTheBestPony <outbackgaminghd@hotmail.com>
2023-01-16 23:39:00 +1100

    Remove old DB calls in update.js and remove irrelevant package.json value

--- src/update.js
+++ src/update.js
@@ -2,5 +2,6 @@ import {dbConnect} from './db/db.js';
 
 console.log("----START OF EXECUTION OF update.js----");
 let dbCon = dbConnect();
-dbCon.querySync("UPDATE p_broadcasts SET broadcast_valid = '1';");
+await dbCon.query("UPDATE p_broadcasts SET broadcast_valid = '1';");
+await dbCon.end();
 console.log("----END OF EXECUTION OF update.js----")
